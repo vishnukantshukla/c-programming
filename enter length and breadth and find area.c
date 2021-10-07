b462dca55f7577b777d5f9cc7823859de2b57b8e
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2021-10-08 01:30:34 +0530

    Add files via upload

---
+++ enter length and breadth and find area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	float length,breadth,area rectangle;
+	scanf("%f%f",&length,&breadth);
+	area rectangle=length*breadth;
+	printf("%f,%f\n",length,breadth);
+	printf("%f",area rectangle);
+	return 0;
+}
