86ef1d821d11da44c21af329ca07c2a1aadaf38c
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2021-10-08 01:35:23 +0530

    Update enter length and breadth and find area.c

--- enter length and breadth and find area.c
+++ enter length and breadth and find area.c
@@ -3,7 +3,7 @@ int main()
 {
 	float length,breadth,area;
 	scanf("%f%f",&length,&breadth);
-	area=length*breadth;
+	area=length*breadth; // area of rectangle;
 	printf("%f,%f\n",length,breadth);
 	printf("%f",area);
 	return 0;
