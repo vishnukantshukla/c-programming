022502b3bf3a6ab9c53c5c71c0db0587f234f4c7
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2021-10-09 20:26:40 +0530

    Add files via upload

---
+++ enter any number and find its root.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  float num,root;
+ scanf("%f",&num);
+ root=sqrt(num);
+ printf("enter any number to find the square root of any number\t:\t%f\nsquare root is\t:\t%f",num,root);
+ return 0;
+}
