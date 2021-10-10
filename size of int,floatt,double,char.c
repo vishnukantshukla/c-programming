8bee1ae74936c92305ad2d51a8a2f5d2bf43c5e6
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2021-10-10 23:00:30 +0530

    Add files via upload

---
+++ size of int,floatt,double,char.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int integertype;
+ float floattype;
+ char chartype;
+ double doubletype;
+printf(" size of int is : %ld\n",sizeof (integertype));
+printf(" size of float is : %ld\n", sizeof( floattype));
+printf(" size of char is : %ld\n", sizeof (chartype));
+printf(" size of double is : %ld\n",sizeof (doubletype));
+return 0;
+}
