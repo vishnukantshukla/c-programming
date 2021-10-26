5d5aa9bb7c4da1e16119eec3c4dacae127eb27a9
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2021-10-27 01:17:47 +0530

    Add files via upload

---
+++ find the size of int,char,float.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int x;
+  char y;
+  float z;
+  printf("The size of integer is = %d\n",sizeof (x));
+  printf("The size of char is = %d\n",sizeof (y));
+  printf("The size of float is = %d\n",sizeof (z));
+  return 0;
+}
+
