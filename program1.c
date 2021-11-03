763b4da67bc0ab3681d4a2c37265a116d446977f
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2021-11-03 11:51:52 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main()
+{
+   int x,y;
+   scanf("%d", & x);
+   scanf("%d", & y);
+   if(x>y)
+   printf(" X IS GREATER");
+   else
+   printf(" Y IS GREATER");
+   return 0;
+}
