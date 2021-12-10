c931526d78fbe7ecbd4d917d6e39027484da4810
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2021-12-10 21:39:46 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<=n-1;i++)
+	{
+	scanf("%d",&a[i]);
+	
+	}
+	for(i=0;i<=n-1;i++)
+	{
+		printf("%d",a[i]);
+	}
+	return  0;
+}
