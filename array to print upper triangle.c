6ea1855db7433b0546276bccff015f4a36156d21
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2021-12-22 20:28:46 +0530

    Add files via upload

---
+++ array to print upper triangle.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	scanf("%d",&n);
+	int x[n][n];
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&x[i][j]);
+		}
+	}
+		
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+			if(i+j==2 || i+j==1 || i+j==0)
+		printf("%d\t",x[i][j]);
+	
+	printf("\n");
+}
+	return 0;
+}
