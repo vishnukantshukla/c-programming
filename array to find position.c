319beb82a97076e1b0533fb5aeef15ded6967386
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2021-12-30 00:44:57 +0530

    Add files via upload

---
+++ array to find position.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,a,flag;
+	printf("Enter the size of Array");
+	scanf("%d",&n);
+	int x[n];
+	printf("Enter the elements");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&x[i]);
+	}
+	scanf("%d",&a);
+		for(i=0;i<n;i++)
+	{
+		if(x[i]==a)
+		printf("The position of elements = %d\n",i+1);
+		flag=flag+1;	
+	}
+	
+	if(flag==0)
+	{
+		printf("Element not present");
+	}
+	return 0;
+}
